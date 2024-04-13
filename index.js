91813cc833b47f929126b72719c91ee5059f6e89
Anurag Rai <anuragrai2222003@gmail.com>
2024-04-14 00:27:55 +0530

    github data added

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 require('dotenv').config()
 const express = require("express"); // module import
 const app = express(); // factory function
-const port = 3000;
+const port = 4000;
+const message = {
+    "message": "Hello there, wayfaring stranger. If you’re reading this then you probably didn’t see our blog post a couple of years back announcing that this API would go away: http://git.io/17AROg Fear not, you should be able to get what you need from the shiny new Events API instead.",
+    "documentation_url": "https://docs.github.com/v3/activity/events/#list-public-events-performed-by-a-user"
+}
 
+app.get('/message', (req, res)=> {
+    res.json(message);
+})
 app.get('/', (req, res) => { // get request
     res.send("Hello world!");
 });
